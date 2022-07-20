1c7d2767356e822758ec005a87113892e3fb4a3e
andynavy23 <andynavy23@gmail.com>
2022-07-21 01:46:17 +0800

    Update user.py
    Modify user_id type

--- schemas/user.py
+++ schemas/user.py
@@ -2,7 +2,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 class User(BaseModel):
-    user_id: str
+    user_id: int
     name: str
     email: Optional[str] = None
     phone: Optional[str] = None
